61e3ba42bc11c61e02a9ed7695abecfbbb294592
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2026-05-03 19:39:33 +0530

    Create message model with schema for MongoDB

---
+++ backend/models/message.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  orderId: String,
+  sender: String,
+  text: String,
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
